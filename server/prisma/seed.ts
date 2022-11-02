f4f030aa8cec84464c8350afb0bb7082e8d8eeff
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-11-02 16:18:34 -0300

    feat(database): add seeds to the database

---
+++ server/prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+	const user = await prisma.user.create({
+		data: {
+			name: 'John DOe',
+			email: 'johndoe@gmail.com',
+			avatarUrl: 'https://github.com/erik-albuquerque.png',
+		},
+	})
+
+	const pool = await prisma.pool.create({
+		data: {
+			title: 'Example Pool',
+			code: 'BOL123',
+			ownerId: user.id,
+			participants: {
+				create: {
+					userId: user.id,
+				},
+			},
+		},
+	})
+
+	await prisma.game.create({
+		data: {
+			date: '2022-11-02T19:10:34.371Z',
+			firstTeamCountryCode: 'DE',
+			secondTeamCountryCode: 'BR',
+		},
+	})
+
+	await prisma.game.create({
+		data: {
+			date: '2022-11-03T19:10:34.371Z',
+			firstTeamCountryCode: 'BR',
+			secondTeamCountryCode: 'AR',
+
+			guesses: {
+				create: {
+					firstTeamPoints: 2,
+					secondTeamPoints: 1,
+					participant: {
+						connect: {
+							userId_poolId: {
+								userId: user.id,
+								poolId: pool.id,
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+}
+
+main()
