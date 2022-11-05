d1a356b8c286f07f1e72752e684ec29b99fe889e
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-11-05 15:37:06 -0300

    feat: add Button component

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,17 @@
-import { Center } from "native-base";
-
+import { Center, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
 import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
       <Logo />
+      <Button
+        title="Entrar com Google"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+      />
     </Center>
   );
 }
